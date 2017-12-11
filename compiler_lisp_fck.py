8038992fe17cd0756a7b77949bef700903ae2944
GeovanniOj <geovannirock@gmail.com>
2017-12-10 23:32:46 -0200

    compiler function done

--- compiler_lisp_fck.py
+++ compiler_lisp_fck.py
@@ -59,3 +59,86 @@ parser = ox.make_parser([
     ('atom : NUMBER', int),
     ('atom : DEF', lambda x: x),
 ], tokens_list)
+
+# definition of functions
+
+def do_before(command, array):
+    new_array = []
+    iterator = 0
+    while iterator < len(array):
+        new_array.append(array[iterator])
+        new_array.append(command)
+        if array[iterator] == 'add' or array[iterator] == 'sub':
+            iterator += 1
+            new_array.append(array[iterator])
+        iterator += 1
+
+    return new_array
+
+def do_after(command, array):
+    new_array = []
+    iterator = 0
+    while iterator < len(array):
+        if array[iterator] == 'add' or array[iterator] == 'sub':
+            new_array.append(array[iterator])
+            iterator += 1
+        new_array.append(array[iterator])
+        new_array.append(command)
+        iterator += 1
+
+    return new_array
+
+
+def add_sub(char, number, list_out):
+    iterator = 0
+    while iterator < number:
+        list_out.append(char)
+        iterator += 1
+
+    return list_out
+
+def lisp_f_ck_compiler(tree, output_list):
+    loop_active = False
+    interactor = 0
+    while interactor < len(tree):
+        if isinstance(tree[interactor], tuple):
+            output_list = lisp_f_ck_compiler(tree[interactor], output_list)
+        elif tree[interactor] == 'inc':
+            output_list.append('+')
+        elif tree[interactor] == 'dec':
+            output_list.append('-')
+        elif tree[interactor] == 'right':
+            output_list.append('>')
+        elif tree[interactor] == 'left':
+            output_list.append('<')
+        elif tree[interactor] == 'add':
+            interactor += 1
+            output_list = add_sub('+', tree[interactor], output_list)
+        elif tree[interactor] == 'sub':
+            interactor += 1
+            output_list = add_sub('-', tree[interactor], output_list)
+        elif tree[interactor] == 'print':
+            output_list.append('.')
+        elif tree[interactor] == 'read':
+            output_list.append(',')
+        elif tree[interactor] == 'do-after':
+            interactor += 1 
+            command = tree[interactor]
+            interactor += 1 
+            array = do_after(command, list(tree[interactor]))
+            output_list = lisp_f_ck_compiler(array, output_list)
+        elif tree[interactor] == 'do-before':
+            interactor += 1 
+            command = tree[interactor]
+            interactor += 1 
+            array = do_before(command, list(tree[interactor]))
+            output_list = lisp_f_ck_compiler(array, output_list)
+        elif tree[interactor] == 'loop':
+            output_list.append('[')
+            output_list = lisp_f_ck_compiler(tree[interactor], output_list)
+            output_list.append(']')
+        elif tree[interactor] == 'def':
+            pass
+        interactor += 1
+
+    return output_list
